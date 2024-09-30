4849a5a90efca2804d8f7ca5ededde04b5e2b34b
湯易鑫 <tangyixin@tangyixindebijixingdiannao.local>
2024-09-30 15:58:00 +0800

    [fix]adjust header display

--- streamlive/src/main/resources/static/assets/js/header.js
+++ streamlive/src/main/resources/static/assets/js/header.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function(){
-    // Smooth Scroll for anchor links
+
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
@@ -8,13 +8,14 @@ document.addEventListener('DOMContentLoaded', function(){
             });
         });
     });
-
     // 登入顯示當前用戶名字
     document.getElementById('user-name').textContent="嗨,"+localStorage.getItem("userName");
 
+    // 載入當前使用者的頭像
+    document.querySelector('.icon-user').src = `${localStorage.getItem("userImage")}`;
+
     // 使用者頭像綁定監聽事件(顯示隱藏視窗)
     document.querySelector('.icon-user').addEventListener('click', function() {
-        console.log("點擊");
         var infoWindow = document.querySelector('header .user-info .info-window');
         if (infoWindow.style.display === 'block') {
             infoWindow.style.display = 'none';
@@ -23,9 +24,14 @@ document.addEventListener('DOMContentLoaded', function(){
         }
     });
 
-    // 綁定導向我的合約按鈕
+    // 合約紀錄
     document.getElementById('my-contract-btn').addEventListener('click',function (){
-        window.location.href="my-contract.html";
+        window.location.href = "my-contract.html";
+    });
+
+    // 直播紀錄
+    document.getElementById('my-livestream-btn').addEventListener('click',function (){
+        window.location.href = "live-stream-records";
     });
 
     // 綁定登出功能
@@ -33,10 +39,11 @@ document.addEventListener('DOMContentLoaded', function(){
         localStorage.removeItem('userEmail');
         localStorage.removeItem('userId');
         localStorage.removeItem('userName');
+        localStorage.removeItem('userImage');
         window.location.href="login.html";
     });
 
-    document.querySelector('.logo').addEventListener('click',function (){
+    document.querySelector('.header-logo').addEventListener('click',function (){
         window.location.href="index.html";
     });
 });
